f5b2b2d261562a805ec19e254cda678bb08cc281
Cooper Garren <cgarren18@icloud.com>
2025-10-13 19:03:00 -0400

    Initial Commit

---
+++ my-maps/my_mapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_mapsApp.swift
+//  my-maps
+//
+//  Created by Cooper Garren on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct my_mapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
